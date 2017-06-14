3526d3cacc9a64b63007448e2c34df86da26b605
Martolins <martaburocchi@gmail.com>
2017-06-14 18:08:10 +0200

    listaQuadri

--- progetto/src/main/java/it/uniroma3/progetto/controller/MainController.java
+++ progetto/src/main/java/it/uniroma3/progetto/controller/MainController.java
@@ -1,12 +1,26 @@
 package it.uniroma3.progetto.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.uniroma3.progetto.model.Autore;
+import it.uniroma3.progetto.model.Quadro;
+import it.uniroma3.progetto.service.QuadroService;
 
 // controller to access the login page
 @Controller
 public class MainController {
+
+	@Autowired
+	private QuadroService quadroService; 
+
 
 	// Login form
 	@RequestMapping("/login")
@@ -20,4 +34,26 @@ public class MainController {
 		model.addAttribute("loginError", true);
 		return "login";
 	}
+
+	//Lista di quadri
+	@GetMapping(value="/listaQuadri")
+	public String showForm(Model model){
+		List<Quadro> quadri = (List<Quadro>) quadroService.findAll(); 
+		model.addAttribute("quadri",quadri);
+		return "listaQuadri";
+	}
+
+//	@GetMapping(value = "/dettagli")
+//	public String dettagliQuadro(@ModelAttribute("id") Long id, BindingResult results, Model model){
+////		
+////		if(results.hasErrors()){
+////			return "listaQuadri";
+////		}
+//		
+//		Quadro quadro = quadroService.findbyId(id);
+//		model.addAttribute(quadro);
+//		return "dettagliQuadro";
+//	}
+	
+
 }
--- progetto/src/main/java/it/uniroma3/progetto/security/SecurityConfig.java
+++ progetto/src/main/java/it/uniroma3/progetto/security/SecurityConfig.java
@@ -1,5 +1,5 @@
 package it.uniroma3.progetto.security;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,34 +13,37 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
- 
-    @Autowired
-    private DataSource dataSource;
- 
+
+	@Autowired
+	private DataSource dataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-		.withUser("user").password("user").roles("USER");
+		.withUser("user").password("user").roles("ADMIN");
 
-	auth.jdbcAuthentication().dataSource(dataSource);
-//		
-//		.passwordEncoder(new BCryptPasswordEncoder())
-//		.usersByUsernameQuery("SELECT username,password,1 FROM users where username=?")
-//		.authoritiesByUsernameQuery("SELECT username,authority FROM authorities where username=?");
+		auth.jdbcAuthentication().dataSource(dataSource);
+
+		//		
+		//		.passwordEncoder(new BCryptPasswordEncoder())
+		//		.usersByUsernameQuery("SELECT username,password,1 FROM users where username=?")
+		//		.authoritiesByUsernameQuery("SELECT username,authority FROM authorities where username=?");
 	}
- 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
 
-        http
-        .authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-        .formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .and()
-        .logout()
-           .permitAll();
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http
+		.authorizeRequests()
+			.antMatchers("/listaQuadri").permitAll()
+			.antMatchers("/formQuadro" , "/formAutore","/index").access("hasAuthority('ADMIN')")
+			.anyRequest().authenticated()
+		.and()
+		.formLogin()
+			.loginPage("/login")
+			.permitAll()
+		.and()
+		.logout()
+			.permitAll();
     }
 }
