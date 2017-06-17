8bb35883ddffbdc38b1dcc517713fd7a890bb8ad
Martolins <martaburocchi@gmail.com>
2017-06-17 12:00:34 +0200

    img da file

--- progetto/src/main/java/it/uniroma3/progetto/controller/QuadroController.java
+++ progetto/src/main/java/it/uniroma3/progetto/controller/QuadroController.java
@@ -1,8 +1,6 @@
 package it.uniroma3.progetto.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
