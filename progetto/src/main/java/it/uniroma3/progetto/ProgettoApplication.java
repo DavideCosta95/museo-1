0a7996fd8ae077e91e35a15317bc874b7131441e
Dashish <Dashish@10.25.201.222>
2017-06-17 12:05:15 +0200

    test

--- progetto/src/main/java/it/uniroma3/progetto/ProgettoApplication.java
+++ progetto/src/main/java/it/uniroma3/progetto/ProgettoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProgettoApplication {
+public class ProgettoApplication {//ciao
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgettoApplication.class, args);
