45a54848e0a3589d59e6dc2e5839e4c0f120e278
Martolins <martaburocchi@gmail.com>
2017-06-17 16:14:47 +0200

    css index

--- progetto/src/main/java/it/uniroma3/progetto/ProgettoApplication.java
+++ progetto/src/main/java/it/uniroma3/progetto/ProgettoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProgettoApplication {//ciao
+public class ProgettoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgettoApplication.class, args);
