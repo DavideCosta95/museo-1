e5b44a2fdd10009143b06db2cc071265aa0083e4
Martolins <martaburocchi@gmail.com>
2017-06-18 15:11:06 +0200

    css

--- progetto/src/main/java/it/uniroma3/progetto/controller/MainController.java
+++ progetto/src/main/java/it/uniroma3/progetto/controller/MainController.java
@@ -2,7 +2,6 @@ package it.uniroma3.progetto.controller;
 
 import java.util.List;
 
-import org.hibernate.annotations.Fetch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
